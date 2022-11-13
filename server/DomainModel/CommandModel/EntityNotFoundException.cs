7e0a54a4724c34da641275952652abe87178150f
Gyuwon Yi <gyuwon@live.com>
2022-11-13 23:08:11 +0900

    Refactor class EntityNotFoundException

--- server/DomainModel/CommandModel/EntityNotFoundException.cs
+++ server/DomainModel/CommandModel/EntityNotFoundException.cs
@@ -1,4 +1,3 @@
-using System.Text;
 using Foundation;
 
 namespace CommandModel;
@@ -6,27 +5,10 @@ namespace CommandModel;
 public class EntityNotFoundException : InvalidOperationException
 {
     public EntityNotFoundException(string source, string subject)
-        : base(ComposeMessage(source, subject))
+        : base($"Could not find entity. Source: {source}, Subject: {subject}")
         => Error = new("EntityNotFound", Message);
 
     public ServiceError Error { get; }
-
-    private static string ComposeMessage(string source, string subject)
-    {
-        StringBuilder builder = new();
-
-        builder.AppendLine("Could not find entity");
-        builder.AppendLine(FormatSource(source));
-        builder.AppendLine(FormatSubject(subject));
-
-        return builder.ToString();
-    }
-
-    private static string FormatSource(string? source)
-        => $"Source: {source}";
-
-    private static string FormatSubject(string? subject)
-        => $"Subject: {subject}";
 
     public static EntityNotFoundException Create<T>(Guid id)
         => new(source: typeof(T).Name, subject: $"{id}");
