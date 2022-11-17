8a528101378e78d29df8f5cd7a06ee15fc0787ff
Vader Yi <vader.yi@healingpaper.com>
2022-11-17 14:15:13 +0900

    Extract <TodoItemListView/>

--- console/app/pages/index.tsx
+++ console/app/pages/index.tsx
@@ -4,12 +4,11 @@ import { AxiosTodoItemReader } from "todos.http";
 import axios from "axios";
 
 axios.defaults.baseURL = "https://localhost:7180/"; 
-const reader: ITodoItemReader = new AxiosTodoItemReader(axios);
 
 export default function App() {
   return (
     <div>
-      <TodoItemListView reader={reader} />
+      <TodoItemListView reader={new AxiosTodoItemReader(axios)} />
     </div>
   );
 }
--- console/todos/views/index.tsx
+++ console/todos/views/index.tsx
@@ -1,6 +1,2 @@
-import * as React from "react";
-import { ITodoItemReader } from "todos.models"
-
-export const TodoItemListView = (props: {
-  reader: ITodoItemReader
-}) => <p>hello world</p>;
+import { TodoItemListView } from "./TodoItemListView";
+export { TodoItemListView };
